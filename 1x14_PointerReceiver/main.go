b1f85011dffd4325c302eb73653ca89a39c72158
juandreww <juandreww@outlook.com>
2022-10-02 13:17:08 +0700

    asdfasf

--- 1x14_PointerReceiver/main.go
+++ 1x14_PointerReceiver/main.go
@@ -13,6 +13,7 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+// kalo ga pake pointer maka diasumsikan cuma nilai copy nya saja
 func (v *Vertex) Scale(f float64) {
 	fmt.Println(v)
 	v.X = v.X * f
